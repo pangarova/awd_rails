3e94e3641a7b4b55aef4cfc3f36f7cf47d25bb23
pangarova <maria.pangarova@gmail.com>
2017-07-25 22:26:49 +0300

    Add Authenticating Users

--- depot/config/routes.rb
+++ depot/config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'admin' => 'admin#index'
+
+  controller :sessions do
+    get  'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
   resources :users
   resources :orders
   resources :line_items
@@ -9,5 +17,6 @@ Rails.application.routes.draw do
     get :who_bought, on: :member
   end
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see
+  # http://guides.rubyonrails.org/routing.html
 end
